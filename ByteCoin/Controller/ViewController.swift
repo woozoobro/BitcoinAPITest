97873e1e14b4daef916ed434385ce330002ced3b
woozoobro <woojusm@gmail.com>
2022-10-17 06:20:48 +0900

    bitcoin First

---
+++ ByteCoin/Controller/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  ByteCoin
+//
+
+
+import UIKit
+
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+ 
+    let coinManager = CoinManager()
+    
+    @IBOutlet weak var bitcoinLabel: UILabel!
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        currencyPicker.dataSource = self
+        currencyPicker.delegate = self
+    }
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
+    }
+}
+
